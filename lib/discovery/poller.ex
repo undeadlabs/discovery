c6ce55aa5386f59724a070feb7d4131fabce221e
Jamie Winsor <jamie@vialstudios.com>
2014-06-20 13:59:34 -0700

    gracefully handle failures from poller task

--- lib/discovery/poller.ex
+++ lib/discovery/poller.ex
@@ -6,6 +6,8 @@ defmodule Discovery.Poller do
   use GenServer
   import Consul.Response, only: [consul_index: 1]
 
+  @retry_ms 5000
+
   def start_link(service) when is_binary(service) do
     GenServer.start_link(__MODULE__, [service])
   end
@@ -17,8 +19,7 @@ defmodule Discovery.Poller do
 
   @spec poll(pid, binary | integer, binary) :: :ok
   def poll(poller, index, service) do
-    results = Consul.Catalog.service(index, service)
-    GenServer.call(poller, {:poll_response, results})
+    Consul.Catalog.service(index, service)
   end
 
   @spec subscribe(pid, atom, pid) :: :ok
@@ -49,6 +50,7 @@ defmodule Discovery.Poller do
     {:ok, %{em: em, service: service, nodes: [], index: nil, task: nil}, 0}
   end
 
+  # Seed node-service data and begin polling
   def handle_info(:timeout, %{service: service, index: nil} = state) do
     case Consul.Catalog.service(service) do
       {:ok, %{body: body} = response} ->
@@ -60,21 +62,15 @@ defmodule Discovery.Poller do
         end
         :ok       = notify_change(new_nodes, state)
         new_state = %{state | nodes: new_nodes, index: consul_index(response)}
-      {:error, response} ->
-        new_state = %{state | nodes: [], index: consul_index(response)}
+        task      = async_poll(self, new_state.index, service)
+        {:noreply, %{new_state | task: task}}
+      {:error, _} ->
+        {:noreply, state, @retry_ms}
     end
-
-    task = async_poll(self, new_state.index, service)
-
-    {:noreply, %{new_state | task: task}}
   end
 
-  def handle_info({:DOWN, ref, _, _, _}, %{service: service, index: index, task: %Task{ref: ref}} = state) do
-    task = async_poll(self, index, service)
-    {:noreply, %{state | task: task}}
-  end
-
-  def handle_call({:poll_response, results}, _from, %{service: service} = state) do
+  # Handle results from polling task
+  def handle_info({ref, results}, %{task: %Task{ref: ref}} = state) do
     case results do
       {:ok, %{body: body} = response} ->
         case Discovery.Node.build(body) do
@@ -85,13 +81,21 @@ defmodule Discovery.Poller do
         end
         :ok       = notify_change(new_nodes, state)
         new_state = %{state | nodes: new_nodes, index: consul_index(response)}
-      {:error, response} ->
-        new_state = %{state | nodes: [], index: consul_index(response)}
+        {:noreply, new_state}
+      {:error, _} ->
+        new_state = %{state | index: nil}
+        {:noreply, new_state, @retry_ms}
     end
+  end
 
-    task = async_poll(self, new_state.index, service)
+  # Poller completed
+  def handle_info({:DOWN, ref, _, _, :normal}, %{service: service, index: index, task: %Task{ref: ref}} = state) do
+    {:noreply, %{state | task: async_poll(self, index, service)}}
+  end
 
-    {:reply, :ok, %{new_state | task: task}}
+  # Poller crashed
+  def handle_info({:DOWN, ref, _, _, _}, %{task: %Task{ref: ref}} = state) do
+    {:noreply, %{state | task: nil, index: nil}, @retry_ms}
   end
 
   def handle_call({:subscribe, module, subscriber}, _from, %{em: em, nodes: nodes} = state) do
